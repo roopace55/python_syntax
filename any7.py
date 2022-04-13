020e40a37c091a32dddccbda5271fbb4bda5445a
Rupesh Basnet <82736629+roopace55@users.noreply.github.com>
2022-04-13 14:46:04 -0700

    first commit

---
+++ any7.py
@@ -0,0 +1,15 @@
+from operator import contains
+
+
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+    for num in nums:
+        if nums == 7:
+            return True
+        return False
+
+    # YOUR CODE HERE
+
+
+print("should be true", any7([1, 2, 7, 4, 5]))
+print("should be false", any7([1, 2, 4, 5]))
